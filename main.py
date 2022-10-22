9a7419cb26aa92aa206f7bb4200b067320027b56
macha <macha1405@yahoo.co.jp>
2022-10-23 08:06:57 +0900

    import csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import win32com.client
+import csv
 import pathlib
 
 WORK_DIR = pathlib.Path()
@@ -16,25 +17,12 @@ with open(MAIL_BODY_TEMPLATE_PATH, encoding="utf-8") as f:
 case_name = 'A3カラーレーザープリンター'
 customre_name = '株式会社テスト'
 
-mail_body_header = '''株式会社ジャパネットたかた
-高田　社長様'''
-
-mail_to_list = ['xxx@xxx.com', 'aaa@aaa.com']
-mail_cc_list  = ['yyy@yyy.com']
-mail_bcc_list  = ['zzz@zzz.com']
-
 mail_to_add_list = ['mmm@mmm.com', 'ooo@ooo.com', 'aaa@aaa.com']
 mail_cc_add_list = ['aaa@aaa.com']
 mail_bcc_add_list = ['yyy@yyy.com']
 
 def add_address(address_list, add_list):
     return list(set(address_list + add_list))
-
-
-mail_to_list = add_address(mail_to_list , mail_to_add_list )
-mail_cc_list = add_address(mail_cc_list , mail_cc_add_list )
-mail_bcc_list = add_address(mail_bcc_list , mail_bcc_add_list )
-
 
 def deduplication_address(address_list_1, address_list_2):
     delete_list = list(set(address_list_1) & set(address_list_2))
@@ -43,9 +31,6 @@ def deduplication_address(address_list_1, address_list_2):
     return address_list_2
 
 
-mail_cc_list = deduplication_address(mail_to_list, mail_cc_list)
-mail_bcc_list = deduplication_address(mail_to_list, mail_bcc_list)
-mail_bcc_list = deduplication_address(mail_cc_list, mail_bcc_list)
 
 
 def create_address(address_list):
@@ -61,13 +46,6 @@ def create_address(address_list):
         return address_name
       
 
-
-mail_to = create_address(mail_to_list)
-mail_cc = create_address(mail_cc_list)
-mail_bcc = create_address(mail_bcc_list)
-
-
-
 def create_text(text_tamplate, case_name, customre_name, mail_body_header):
     i = text_tamplate
     i = i.replace('【case_name】', case_name)
@@ -75,8 +53,7 @@ def create_text(text_tamplate, case_name, customre_name, mail_body_header):
     i = i.replace('【mail_body_header】', mail_body_header)
     return i
 
-mail_subject = create_text(MAIL_SUBJECT_TEMPLATE, case_name, customre_name, mail_body_header)
-mail_body = create_text(MAIL_BODY_TEMPLATE, case_name, customre_name, mail_body_header)
+
 
 def create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body):
     # Outlookのmailオブジェクト設定
@@ -92,4 +69,46 @@ def create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body):
     objMail.Display(True) # MailItemオブジェクトを画面表示で確認する
     # objMail.Send() # Mailを即時送信
 
-create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body)
+
+
+with open('mailng_list.csv', encoding='shift-jis') as f:
+    dict_reader = csv.DictReader(f)
+    mailing_list = [row for row in dict_reader]
+
+
+for mail_info in mailing_list:
+    
+    mail_body_header = ''
+    mail_to_list = []
+    mail_cc_list  = []
+    mail_bcc_list  = []
+
+    for i in mail_info :
+        if i == 'header':
+            mail_body_header = mail_info[i]
+        elif i.startswith('to'):
+            if bool(mail_info[i]):
+                mail_to_list.append(mail_info[i])
+        elif i.startswith('cc'):
+            if bool(mail_info[i]):
+                mail_cc_list.append(mail_info[i])
+        elif i.startswith('bcc'):
+            if bool(mail_info[i]):
+                mail_bcc_list.append(mail_info[i])
+
+    mail_to_list = add_address(mail_to_list , mail_to_add_list )
+    mail_cc_list = add_address(mail_cc_list , mail_cc_add_list )
+    mail_bcc_list = add_address(mail_bcc_list , mail_bcc_add_list )
+
+    mail_cc_list = deduplication_address(mail_to_list, mail_cc_list)
+    mail_bcc_list = deduplication_address(mail_to_list, mail_bcc_list)
+    mail_bcc_list = deduplication_address(mail_cc_list, mail_bcc_list)
+
+    mail_to = create_address(mail_to_list)
+    mail_cc = create_address(mail_cc_list)
+    mail_bcc = create_address(mail_bcc_list)
+
+    mail_subject = create_text(MAIL_SUBJECT_TEMPLATE, case_name, customre_name, mail_body_header)
+    mail_body = create_text(MAIL_BODY_TEMPLATE, case_name, customre_name, mail_body_header)
+
+    create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body)
