959430d7fbc64f753ef9c4fc0d0698b746d6091c
macha <macha1405@yahoo.co.jp>
2022-10-22 12:37:48 +0900

    create_text

--- test.py
+++ test.py
@@ -1,12 +1,43 @@
 import win32com.client
+import re
+
+case_name = 'A3カラーレーザープリンター'
+customre_name = '株式会社テスト'
+mail_body_header = '''株式会社ジャパネットたかた
+高田　社長様'''
 
 mail_to = 'xxx@xxx.com; aaa@aaa.com' 
 mail_cc = 'yyy@yyy.com'
 mail_bcc = 'zzz@zzz.com' 
-mail_subject = 'title'
-mail_body = 'mail body test + sign'
+mail_subject_template = '【お見積依頼】【case_name】　【customre_name】さま分'
+mail_body_tamplate = '''【mail_body_header 】
+
+この度は、【case_name】のお見積りをお願いしたくご連絡いたしました。
+
+お客さま: 【customer_name】さま
+
+以上です。
+
+よろしくお願いいたします。'''
+
 #objMail.Attachments.Add('hogehoge.csv') # 送付ファイルがある場合はファイルパスで添付
 
+def create_text(tamplate, case_name, customre_name, mail_body_header):
+    i = tamplate.replace('【case_name】', case_name)
+    i = i.replace('【customre_name】', customre_name)
+    i = i.replace('【mail_body_header】', mail_body_header)
+    return i
+
+
+
+
+
+
+
+mail_body = create_text(mail_body_tamplate, case_name, customre_name, mail_body_header)
+
+
+mail_subject = create_text(mail_subject_template, case_name, customre_name, mail_body_header)
 
 def create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body):
     # Outlookのmailオブジェクト設定
