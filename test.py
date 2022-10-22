75abc294a7ac2bb9ba33a6b1ac83613b0c67406f
macha <macha1405@yahoo.co.jp>
2022-10-22 11:19:57 +0900

    create_def

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-print(1)
+import win32com.client
+
+mail_to = 'xxx@xxx.com; aaa@aaa.com' 
+mail_cc = 'yyy@yyy.com'
+mail_bcc = 'zzz@zzz.com' 
+mail_subject = 'title'
+mail_body = 'mail body test + sign'
+#objMail.Attachments.Add('hogehoge.csv') # 送付ファイルがある場合はファイルパスで添付
+
+
+def create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body):
+    # Outlookのmailオブジェクト設定
+    outlook = win32com.client.Dispatch("Outlook.Application")
+    objMail = outlook.CreateItem(0) # MailItemオブジェクトのID
+
+    objMail.To = mail_to
+    objMail.cc = mail_cc
+    objMail.Bcc = mail_bcc
+    objMail.Subject = mail_subject
+    objMail.Body = mail_body
+
+    objMail.Display(True) # MailItemオブジェクトを画面表示で確認する
+    # objMail.Send() # Mailを即時送信
+
+
+
+
+create_mail(mail_to, mail_cc, mail_bcc, mail_subject, mail_body)
